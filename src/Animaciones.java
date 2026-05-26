9f7b3b2478cbf171fa007d21646ba2b0117768de
Aether <lucerosamuel1@gmail.com>
2026-05-25 20:50:13 -0500

    Implementacion de Clases Animaciones y CPU

--- src/Animaciones.java
+++ src/Animaciones.java
@@ -1,6 +1,20 @@
 public class Animaciones {
     public static void mostrarCuentaRegresiva() {
-        System.out.println("3... 2... 1...");
+        System.out.print("Preparando batalla");
+        for (int i = 3; i > 0; i--) {
+            System.out.print(" " + i);
+            esperar(400);
+        }
+        System.out.println(" ¡YA!");
+    }
+
+    public static void mostrarBarraCargando(String mensaje) {
+        System.out.print(mensaje);
+        for (int i = 0; i < 5; i++) {
+            System.out.print(".");
+            esperar(200);
+        }
+        System.out.println();
     }
 
     public static void esperar(int millis) {
--- src/CPU.java
+++ src/CPU.java
@@ -1,9 +1,16 @@
+import java.util.Random;
+
 public class CPU extends Jugador {
+    private static final String[] OPCIONES = {"piedra", "papel", "tijera"};
+    private final Random random;
+
     public CPU(String nombre, int vidas) {
         super(nombre, vidas);
+        this.random = new Random();
     }
 
     public void seleccionarMovimiento() {
-        // Selección de movimiento de la CPU.
+        int indice = random.nextInt(OPCIONES.length);
+        setEleccionActual(OPCIONES[indice]);
     }
 }
